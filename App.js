64c25ac6e0ecf7df8b7736e6041c545e17716d99
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2021-01-31 13:56:40 +0100

    ADD form

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import {
   Image,
 } from 'react-native';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 const App = () => {
   return (
@@ -18,6 +19,9 @@ const App = () => {
         style={styles.imagen}
         source={require('./assets/img/cryptomonedas.png')}
       />
+      <View style={styles.contenido}>
+        <Formulario />
+      </View>
     </>
   );
 };
@@ -26,6 +30,9 @@ const styles = StyleSheet.create({
   imagen: {
     width: '100%',
     height: 150,
+    marginHorizontal: '2.5%',
+  },
+  contenido: {
     marginHorizontal: '2.5%',
   },
 });
